d47df765da3ce14407b0a73518099ad02789a175
Hindu <rajuhindur@gmail.com>
2024-12-04 11:49:06 +0400

    Add files via upload

---
+++ Check2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Check2
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+          int a=sc.nextInt();
+          int b=sc.nextInt();
+        if(a==b)
+        {
+            System.out.println("a is equal to b");
+        }
+        else
+        {
+            System.out.println("a is not equql to b");
+        }
+    }   
+}
