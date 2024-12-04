b326c944e56487dba250c28c89c6b18b8809b449
Hindu <rajuhindur@gmail.com>
2024-12-04 11:50:29 +0400

    Add files via upload

---
+++ Checkpower.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Checkpower
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+          int a=sc.nextInt();
+          if((a&(a-1))==0)
+          {
+          System.out.println("power of 2");
+          }
+           else
+           {
+           System.out.println("not power of 2");
+           }
+    }   
+}
